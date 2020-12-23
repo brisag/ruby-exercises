5d5a0c57328e9595ee1144bccd004b626fe7f30f
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-23 12:13:55 -0600

    Update Exersice code

--- enumerables/exercises_1/select_pattern_test.rb
+++ enumerables/exercises_1/select_pattern_test.rb
@@ -17,37 +17,52 @@ class SelectPatternTest < Minitest::Test
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = []
     numbers.each do |number|
-      # Your code goes here
+      odds << number if number.odd?
     end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
+    # skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     selected = []
+    words.each do |word|
+      selected << word if word.length == 3
+    end
     # Your code goes here
     assert_equal ["bad", "cat", "dog", "red"], selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
+    # skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
+    selected = []
+    words.each do |word|
+      selected << word if word.length > 3
+    end
     # Your code goes here
     assert_equal ["pill", "finger", "blue", "table"], selected
   end
 
   def test_pick_words_ending_in_e
-    skip
+    # skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word[-1] == 'e'
+    end
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
+    # skip
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
     # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word[-3..-1] == "ing"
+    end
     assert_equal ["bring", "singing"], selected
   end
 
@@ -55,6 +70,9 @@ class SelectPatternTest < Minitest::Test
     skip
     words = ["four", "red", "five", "blue", "pizza", "purple"]
     # Your code goes here
+    words.each do |word|
+      selected << word if word.include? "e"
+    end
     assert_equal ["red", "five", "blue", "purple"], selected
   end
 
@@ -62,12 +80,20 @@ class SelectPatternTest < Minitest::Test
     skip
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
     # Your code goes here
+    dinosaurs = []
+    animals.each do |animal|
+      dinosaurs << animal if animal.include? "saurus"
+    end
     assert_equal ["tyrannosaurus", "achillesaurus", "qingxiusaurus"], dinosaurs
   end
 
   def test_pick_floats
-    skip
+    # skip
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
+    floats = []
+    numbers.each do |number|
+      floats << number if number.class == Float
+    end
     # Your code goes here
     assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
   end
@@ -75,6 +101,7 @@ class SelectPatternTest < Minitest::Test
   def test_pick_arrays
     skip
     elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
+
     # Your code goes here
     assert_equal [["dog"], [56, 3, 8]], arrays
   end
@@ -83,6 +110,10 @@ class SelectPatternTest < Minitest::Test
     skip
     elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
     # Your code goes here
+    hashes = []
+    elements.each do |element|
+      hashes << element if element.class == Hash
+    end
     assert_equal [{:dog=>"fido"}, {:stuff=>"things"}], hashes
   end
 
